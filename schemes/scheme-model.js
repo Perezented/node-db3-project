5a1a374010ea8a21539690417c50bc22be5a137c
Michael Perez <mperez6473@gmail.com>
2020-06-10 13:51:14 -0500

    Have the by id and findSteps working nicely

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -13,10 +13,12 @@ function find() {
     return db("schemes");
 }
 function findById(id) {
-    return db("schemes").where("id", id);
+    return db("schemes").where("id", id).first();
 }
 function findSteps(id) {
-    return db("steps").where("scheme_id", id);
+    return db("steps")
+        .where("scheme_id", id)
+        .join("schemes", "steps.scheme_id", "schemes.id");
 }
 function add(data) {
     return db("schemes").insert(data, "*");
